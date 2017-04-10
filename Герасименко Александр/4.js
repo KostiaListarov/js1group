28f0a7c2327a1b28ab929de667a3d5fb482540bb
alex-gera <sanya9911@icloud.com>
2017-04-10 21:28:01 +0300

    Урок 2. Задание 4

---
+++ Герасименко Александр/4.js
@@ -0,0 +1,59 @@
+  /*Присвоить переменной а значение в промежутке [0..15]. 
+  С помощью оператора switch организовать вывод чисел от a до 15. */
+
+var a = 10,
+    string = "";
+
+switch (a) {
+  case 1:
+    string = ("1 ");
+    a++;
+  case 2:
+    string += ("2 ");
+    a++;
+  case 3:
+    string += ("3 ");
+    a++;
+  case 4:
+    string += ("4 ");
+    a++;
+  case 5:
+    string += ("5 ");
+    a++;
+  case 6:
+    string += ("6 ");
+    a++;
+  case 7:
+    string += ("7 ");
+    a++;
+  case 8:
+    string += ("8 ");
+    a++;
+  case 9:
+    string += ("9 ");
+    a++;
+  case 10:
+    string += ("10 ");
+    a++;
+  case 11:
+    string += ("11 ");
+    a++;
+  case 12:
+    string += ("12 ");
+    a++;
+  case 13:
+    string += ("13 ");
+    a++;
+  case 14:
+    string += ("14 ");
+    a++;
+  case 15:
+    string += ("15");
+    alert(string);
+    break;
+}
+
+
+
+
+
