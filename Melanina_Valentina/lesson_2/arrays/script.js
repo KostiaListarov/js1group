0f7dff44187b2b4e2cb9a4f9c752d0d4702f56e4
ValentinaMelanina <mail@melanina.ru>
2017-04-14 02:20:01 +0300

    Массивы, задание 1

---
+++ Melanina_Valentina/lesson_2/arrays/script.js
@@ -0,0 +1,28 @@
+/**
+ * Created by valentinamelanina on 13.04.17.
+ */
+
+// Добавляем действие по клику
+document.getElementById("show_array").addEventListener("click", array_function, false);
+
+function array_function(e) {
+    e.preventDefault();
+
+    // Создаем массив с 10 разными числами:
+    var array = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
+    var new_array = [],
+        k;
+
+
+    // Вычисляем новое значение для массива new_array
+    for (var i = 0; i < array.length; i++) { // Перебираем весь массив
+        k = Math.pow(array[i],2); // Каждое значение возводим в квадрат
+        new_array[i] = k; // Добавляем результат в новый массив
+
+        // Выводим значения
+       console.log(new_array[i]);
+
+    }
+
+}
+
