a4e7eb22d12cf43d772851892d8ff60a0e343803
ValentinaMelanina <mail@melanina.ru>
2017-04-14 02:58:31 +0300

    Значение нового массива с join + alert

--- Melanina_Valentina/lesson_2/arrays/script.js
+++ Melanina_Valentina/lesson_2/arrays/script.js
@@ -10,19 +10,18 @@ function array_function(e) {
 
     // Создаем массив с 10 разными числами:
     var array = [10, 15, 20, 25, 30, 35, 40, 45, 50, 55];
-    var new_array = [],
-        k;
+    var new_array = [], k,
+    array_result_block = document.getElementsByClassName('array-result');
 
 
     // Вычисляем новое значение для массива new_array
     for (var i = 0; i < array.length; i++) { // Перебираем весь массив
         k = Math.pow(array[i],2); // Каждое значение возводим в квадрат
         new_array[i] = k; // Добавляем результат в новый массив
-
-        // Выводим значения
-       console.log(new_array[i]);
-
     }
+
+    // Выводим значения
+    alert("Новый массив: " + new_array.join(", "));
 
 }
 
